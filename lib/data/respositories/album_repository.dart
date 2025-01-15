0278bf1a57e3c51737fc984460a75c8ed70c51e6
Pawan Kumar <pawan221b@gmail.com>
2025-01-15 16:47:17 +0530

    feat: implement basic repository and tests

---
+++ lib/data/respositories/album_repository.dart
@@ -0,0 +1,41 @@
+import 'package:dio/dio.dart';
+import 'package:infinity_albums/data/models/album.dart';
+import 'package:infinity_albums/data/models/photo.dart';
+
+class AlbumRepository {
+  final Dio _dio;
+
+  // Dummy Url for now as we are just using placeholders
+  final String baseUrl = 'https://jsonplaceholder.typicode.com';
+
+  AlbumRepository([Dio? dio]) : _dio = dio ?? Dio();
+
+  // Get all albums from the API
+  Future<List<Album>> getAlbums() async {
+    try {
+      final response = await _dio.get('$baseUrl/albums');
+      final albums =
+          (response.data as List).map((json) => Album.fromJson(json)).toList();
+
+      return albums;
+    } catch (e) {
+      throw Exception('Failed to load albums - $e');
+    }
+  }
+
+  // Get All Photos for a specific album
+  Future<List<Photo>> getPhotos(int albumId) async {
+    try {
+      final response = await _dio.get('$baseUrl/photos', queryParameters: {
+        'albumId': albumId,
+      });
+
+      final photos =
+          (response.data as List).map((json) => Photo.fromJson(json)).toList();
+
+      return photos;
+    } catch (e) {
+      throw Exception('Failed to load photos - $e');
+    }
+  }
+}
---
+++ test/repositories/album_repository_test.dart
@@ -0,0 +1,75 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:infinity_albums/data/respositories/album_repository.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'album_repository_test.mocks.dart';
+
+@GenerateMocks([Dio])
+void main() {
+  late AlbumRepository albumRepository;
+  late MockDio mockDio;
+
+  const String baseUrl = 'https://jsonplaceholder.typicode.com';
+
+  setUp(() {
+    mockDio = MockDio();
+    albumRepository = AlbumRepository(mockDio);
+  });
+
+  group('Album repository', () {
+    test('getAlbums returns list of albums', () async {
+      when(mockDio.get('$baseUrl/albums')).thenAnswer(
+        (_) async => Response(
+            requestOptions: RequestOptions(
+              path: '$baseUrl/albums',
+            ),
+            data: [
+              {'id': 1, 'title': 'Album 1'},
+            ],
+            statusCode: 200),
+      );
+
+      final albums = await albumRepository.getAlbums();
+      expect(albums.length, 1);
+      expect(albums.first.title, 'Album 1');
+    });
+    test('getPhotosForAlbum returns list of photos', () async {
+      const albumId = 1;
+      when(mockDio.get(
+        'https://jsonplaceholder.typicode.com/photos',
+        queryParameters: {'albumId': albumId},
+      )).thenAnswer((_) async => Response(
+            data: [
+              {
+                'id': 1,
+                'albumId': albumId,
+                'title': 'Photo 1',
+                'url': 'http://example.com/photo1.jpg',
+                'thumbnailUrl': 'http://example.com/thumb1.jpg',
+              }
+            ],
+            statusCode: 200,
+            requestOptions: RequestOptions(
+              path: 'https://jsonplaceholder.typicode.com/photos',
+            ),
+          ));
+
+      final photos = await albumRepository.getPhotos(albumId);
+      expect(photos.length, 1);
+      expect(photos.first.title, 'Photo 1');
+    });
+
+    test('getAlbums throws exception on error', () {
+      when(mockDio.get('https://jsonplaceholder.typicode.com/albums'))
+          .thenThrow(DioException(
+        requestOptions: RequestOptions(
+          path: 'https://jsonplaceholder.typicode.com/albums',
+        ),
+      ));
+
+      expect(() => albumRepository.getAlbums(), throwsException);
+    });
+  });
+}
