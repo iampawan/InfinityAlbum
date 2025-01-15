f5aab38e8703da600531d0d8d424bc4b88b36f5f
Pawan Kumar <pawan221b@gmail.com>
2025-01-15 17:43:45 +0530

    feat: scrollcontroller added for infinite scrolling

--- lib/presentation/widgets/album_list.dart
+++ lib/presentation/widgets/album_list.dart
@@ -12,12 +12,48 @@ class AlbumList extends StatefulWidget {
 }
 
 class _AlbumListState extends State<AlbumList> {
+  late ScrollController _scrollController;
+  late List<Album> _infiniteAlbums;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _infiniteAlbums = List.from(widget.albums);
+    _scrollController.addListener(_onScroll);
+  }
+
+  void _onScroll() {
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.position.pixels;
+
+    if (currentScroll >= maxScroll - 200) {
+      setState(() {
+        _infiniteAlbums.addAll(widget.albums);
+      });
+    }
+
+    if (currentScroll <= 200) {
+      setState(() {
+        _infiniteAlbums.insertAll(0, widget.albums);
+        _scrollController.jumpTo(_scrollController.position.pixels + 100);
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: widget.albums.length,
+      controller: _scrollController,
+      itemCount: _infiniteAlbums.length,
       itemBuilder: (context, index) {
-        final album = widget.albums[index];
+        final album = _infiniteAlbums[index];
         return AlbumItem(album: album);
       },
     );
